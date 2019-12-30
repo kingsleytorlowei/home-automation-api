5cc5f8b3a3dafe6b0bcba79071a3f09bb8ac8b48
Kingsley Torlowei <kingsleytorlowei@gmail.com>
2019-12-30 04:46:38 -0500

    Update settings.py

--- api/settings.py
+++ api/settings.py
@@ -83,14 +83,11 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 DATABASES = {
     'default': {
-    'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    'NAME': 'api',
-    'USER': 'kingsleytorlowei',
-    'PASSWORD': 'CALLOFDUTY11',
-    'HOST': 'localhost',
-    'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 
 # Password validation
